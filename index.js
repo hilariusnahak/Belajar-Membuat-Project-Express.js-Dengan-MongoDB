f8058c17c00ebdb402da0d6b1c8b76af0844a799
hilariusnahak6@gmail.com <hilariusnahak6@gmail.com>
2024-07-10 23:17:58 +0700

    Membuat Restful Update Product

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 // Models
 const Product = require('./models/product');
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://127.0.0.1/shop_db').then((result) => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
  res.send('Hello Ian')
@@ -47,6 +49,12 @@ app.get('/products/:id/edit', async (req, res) => {
  const {id} = req.params;
  const product = await Product.findById(id);
  res.render('products/edit', {product});
+});
+
+app.put('/products/:id', async (req, res) => {
+ const {id} = req.params;
+ const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true });
+ res.redirect(`/products/${product._id}`);
 });
 
 app.listen(port, () => {
