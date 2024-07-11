41ead2e6bbe2a3be0f7d6dfd2b117f9154ae447f
hilariusnahak6@gmail.com <hilariusnahak6@gmail.com>
2024-07-11 18:01:01 +0700

    add: Error Handling Di Async Function

---
+++ README.md
@@ -0,0 +1,10 @@
+# Persiapan Project Express MongoDB
+# Membuat Model dan Seed untuk Data Sample
+# Membuat Restful Index Product
+# Membuat Restful Detail Product
+# Menyiapkan Form Untuk Menyimpan Data Product
+# Membuat Restful Save Product
+# Menyiapkan Form Untuk Edit Product
+# Membuat Restful Update Product
+# Membuat Restful Delete Product
+# Membuat Filter Data Produk Berdasarkan Ketegoi
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const ErrorHandler = require('./ErrorHandler');
 // Models
 const Product = require('./models/product');
 const app = express();
@@ -45,28 +46,49 @@ app.post('/products', async (req, res) => {
  res.redirect(`/products/${product._id}`);
 })
 
-app.get('/products/:id', async (req, res) => {
- const {id} = req.params;
- const product = await Product.findById(id);
- res.render('products/show', {product});
+app.get('/products/:id', async (req, res, next) => {
+ try {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('products/show', {product});
+ } catch (error) {
+  next(new ErrorHandler('Product not found', 404));
+ }
 });
 
-app.get('/products/:id/edit', async (req, res) => {
- const {id} = req.params;
- const product = await Product.findById(id);
- res.render('products/edit', {product});
+app.get('/products/:id/edit', async (req, res, next) => {
+ try {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', {product});
+ } catch (error) {
+  next(new ErrorHandler('Product not found', 404));
+ }
 });
 
-app.put('/products/:id', async (req, res) => {
- const {id} = req.params;
- const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true });
- res.redirect(`/products/${product._id}`);
+app.put('/products/:id', async (req, res, next) => {
+ try {
+  const {id} = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true });
+  res.redirect(`/products/${product._id}`);
+ } catch (error) {
+  next(new ErrorHandler('Gagal update data', 412));
+ }
 });
 
-app.delete('/products/:id', async (req, res) => {
- const {id} = req.params;
- await Product.findByIdAndDelete(id);
- res.redirect('/products');
+app.delete('/products/:id', async (req, res, next) => {
+ try {
+  const {id} = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect('/products');
+ } catch (error) {
+  next(new ErrorHandler('Gagal delete data', 412));
+ }
+})
+
+app.use((err, req, res, next) => {
+ const { status = 500, message = 'Something Went Wrong' } = err;
+ res.status(status).send(message);
 })
 
 app.listen(port, () => {
